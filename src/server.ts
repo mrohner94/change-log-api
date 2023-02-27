045dc389d06953e910e6a349d6229ccae494920e
Mike Rohner <mrohner94@hotmail.com>
2023-02-27 17:53:43 -0500

    Middleware / Routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import express from 'express'
+import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use((req, res, next) => {
+    req.shhhh_secret = 'dont tell'
+    next()
+    // res.status(401)
+    // res.send('Nope')
+})
 
 app.get('/', (req, res) => {
     console.log('get /')
@@ -9,5 +24,7 @@ app.get('/', (req, res) => {
         message: 'hello'
     })
 })
+
+app.use('/api', router)
 
 export default app;
