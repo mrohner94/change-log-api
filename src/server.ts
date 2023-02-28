3432b3cab1aa740dbeb13dc5ce862da36226e318
Mike Rohner <mrohner94@hotmail.com>
2023-02-27 22:07:47 -0500

    User routes

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
 import { protect } from './modules/auth'
+import { createNewUser, signIn } from './handlers/user'
 
 const app = express()
 
@@ -10,13 +11,6 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
-// app.use((req, res, next) => {
-//     req.shhhh_secret = 'dont tell'
-//     next()
-//     // res.status(401)
-//     // res.send('Nope')
-// })
 
 app.get('/', (req, res) => {
     console.log('get /')
@@ -27,5 +21,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api', protect, router)
+
+app.post('/user', createNewUser)
+app.post('/signin', signIn)
 
 export default app;
