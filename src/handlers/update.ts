f5cacc8a4a5f26d5e3a680fe7573d7ee6a4ec984
Mike Rohner <mrohner94@hotmail.com>
2023-03-01 17:15:43 -0500

    update handler, updated routes

---
+++ src/handlers/update.ts
@@ -0,0 +1,103 @@
+import prisma from "../modules/db"
+
+export const getOneUpdate = async (req, res) => {
+    const update = prisma.update.findUnique({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.json({data: update})
+}
+
+export const getUpdates = async (req, res) => {
+    const products = await prisma.product.findMany({
+        where: {
+            belongsToId: req.user.id
+        },
+        include: {
+            updates: true
+        }
+    })
+
+    const updates = products.reduce((allUpdates, products) => {
+        return [...allUpdates, ...products.updates]
+    }, [])
+
+    res.json({data: updates});
+}
+
+export const createUpdate = async (req, res) => {
+    const product = await prisma.product.findUnique({
+        where: {
+            id: req.body.productId
+        }
+    })
+
+    if(!product) {
+        // does not belong to user
+        res.json({message: 'nope'})
+    }
+
+    const update = await prisma.update.create({
+        data: req.body
+    })
+
+    res.json({data: update})
+}
+export const updateUpdate = async (req, res) => {
+    const products = await prisma.product.findMany({
+        where: {
+            belongsToId: req.user.id
+        },
+        include: {
+            updates: true
+        }
+    })
+
+    const updates = products.reduce((allUpdates, products) => {
+        return [...allUpdates, ...products.updates]
+    }, [])
+
+    const match = updates.find(update => update.id === req.params.id)
+
+    if(!match) {
+        return res.json({message: 'Failed update'})
+    }
+    const updatedUpdate = await prisma.update.update({
+        where: {
+            id: req.params.id
+        },
+        data: req.body
+    })
+
+    res.json({data: updatedUpdate})
+}
+export const deleteUpdate = async (req, res) => {
+    const products = await prisma.product.findMany({
+        where: {
+            belongsToId: req.user.id
+        },
+        include: {
+            updates: true
+        }
+    })
+
+    const updates = products.reduce((allUpdates, products) => {
+        return [...allUpdates, ...products.updates]
+    }, [])
+
+    const match = updates.find(update => update.id === req.params.id)
+
+    if(!match) {
+        return res.json({message: 'Failed update'})
+    }
+
+    const deleted = await prisma.update.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.json({data: deleted})
+}
--- src/router.ts
+++ src/router.ts
@@ -1,37 +1,37 @@
 import { Router } from 'express'
 import { body } from 'express-validator'
 import { handleInputErrors } from './modules/middleware'
-import { getProducts, createProduct } from './handlers/product';
+import { getProducts, createProduct, getOneProduct, deleteProduct, updateProduct } from './handlers/product';
+import { getOneUpdate, updateUpdate, getUpdates, createUpdate, deleteUpdate } from './handlers/update';
 
 const router = Router()
 
 // Product
 router.get('/product', getProducts)
-router.get('/product/:id', () => {})
-router.put('/product/:id', body("name").isString(), handleInputErrors, (req, res) => {
-})
+router.get('/product/:id', getOneProduct)
+router.put('/product/:id', body("name").isString(), handleInputErrors, updateProduct)
 router.post('/product', body("name").isString(), handleInputErrors, createProduct)
-
-router.delete('/product/:id', () => {})
+router.delete('/product/:id', deleteProduct)
 
 // Update
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
+router.get('/update', getUpdates)
+router.get('/update/:id', getOneUpdate)
 router.put('/update/:id', 
     body('title').optional().isString(), 
     body('body').optional().isString(), 
     body('status').optional().isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
     body('version').optional().isString(), 
     handleInputErrors, 
-    () => {}
+    updateUpdate
 )
 router.post('/update', 
     body('title').exists().isString(), 
     body('body').exists().isString(), 
+    body('productId').exists().isString(),
     handleInputErrors, 
-    () => {}
+    createUpdate
 )
-router.delete('/update/:id', () => {})
+router.delete('/update/:id', deleteUpdate)
 // Update Point
 router.get('/updatepoint', () => {})
 router.get('/updatepoint/:id', () => {})
