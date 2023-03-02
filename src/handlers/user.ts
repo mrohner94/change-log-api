55079466591c160bf0c30105071a4c1abdb1dee8
Mike Rohner <mrohner94@hotmail.com>
2023-03-01 20:03:48 -0500

    Error handling

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,16 +1,21 @@
 import prisma from '../modules/db'
 import { hashPassword, createJWT, comparePasswords } from '../modules/auth'
 
-export const createNewUser = async (req, res) => {
-    const user = await prisma.user.create({
-        data: {
-            username: req.body.username,
-            password: await hashPassword(req.body.password)
-        }
-    })
-
-    const token = createJWT(user)
-    res.json({ token })
+export const createNewUser = async (req, res, next) => {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                username: req.body.username,
+                password: await hashPassword(req.body.password)
+            }
+        })
+    
+        const token = createJWT(user)
+        res.json({ token })
+    } catch(e) {
+        e.type = 'input'
+        next(e)
+    }
 }
 
 export const signIn = async (req, res) => {
--- src/router.ts
+++ src/router.ts
@@ -51,4 +51,14 @@ router.post('/updatepoint',
 
 router.delete('/updatepoint/:id', () => {})
 
+router.use((err, req, res, next) => {
+    if(err.type === 'auth') {
+        res.status(401).json({message: 'unauthorized'})
+    } else if (err.type === 'input') {
+        res.status(400).json({message: 'invalid input'})
+    } else {
+        res.status(500).json({message: 'oops, thats on us'})
+    }
+})
+
 export default router
